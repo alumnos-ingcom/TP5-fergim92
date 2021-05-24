44e6745d90494ce79475b771b796c437cf0b889d
Fernando Marcos Gimenez <gimenezfernandomarcos@gmail.com>
2021-05-24 14:17:14 -0300

    Ejercicio 3 corregido

--- tp5ej3.py
+++ tp5ej3.py
@@ -2,26 +2,25 @@
 # Gimenez Fernando - @fergim92
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-class IngresoIncorrecto(Exception):
-    pass
-
-def tribonacci(termino):
+from errores import no_numero_no_positivo
+def tribonacci(numero):
     serie = [1,1,1]
-    for i in range(termino -3):
+    for i in range(numero -3):
         serie.append(serie[-1] + serie[-2] + serie[-3])
-    return serie
+     
+    n_esimo = serie[numero -1]                                 ##ultimo elemento elejido por el usuario
+    return n_esimo
 
 
 def prueba():
-    termino = input('Ingrese un numero entero positivo para calcular su serie tribonacci (minimo 3): ')
-    try:
-        termino = int(termino)
-    except ValueError as err:
-        raise IngresoIncorrecto(f'"{termino}" No era un número entero!!!') from err
-    if termino <= 2:
-        raise IngresoIncorrecto(f'"{termino}" Entero positivo y mayor a 2!!!')
+    numero = input('Ingrese un numero entero positivo para calcular su serie tribonacci (minimo 3): ')
+    numero = no_numero_no_positivo(numero)
     
-    print(tribonacci(termino))
+    while numero < 3:
+        numero = input('Mayor a 3!!!: ')
+        numero = no_numero_no_positivo(numero)
+        
+    print(tribonacci(numero))
     
 
 if __name__ == "__main__":
