57dbeddd8949018cb538d0d6eae9408690377b1a
Fernando Marcos Gimenez <gimenezfernandomarcos@gmail.com>
2021-05-24 14:49:35 -0300

    Ejercicio 1 corregido

--- tp5ej1.py
+++ tp5ej1.py
@@ -2,33 +2,24 @@
 # Gimenez Fernando - @fergim92
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-class IngresoIncorrecto(Exception):
-    pass
+from errores import no_numero_int
 
 def par_impar(numero):
-    if numero > 0:
-        while numero > 0:
+    while numero > 0:
             numero -= 2
-        if numero == 0:
-            return True
-        else:
-            return False
-    
-    else:
-        while numero < 0:
+            
+    while numero < 0:
             numero += 2
-        if numero == 0:
-            return True
-        else:
-            return False
+            
+    if numero == 0:
+        return True
+    else:
+        return False
    
 
 def prueba():
     numero = input('Ingrese un numero entero para saber si es par o impar: ')
-    try:
-        numero = int(numero)
-    except ValueError as err:
-        raise IngresoIncorrecto(f'"{numero}" No era un número entero!') from err
+    numero = no_numero_int(numero)
     
     resultado = par_impar(numero)
     if resultado == True:
