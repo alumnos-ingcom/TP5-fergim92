a6d3bcf3415da7cc3ba49c35827fead7be86a885
Fernando Marcos Gimenez <gimenezfernandomarcos@gmail.com>
2021-05-24 13:55:07 -0300

    Ejercicio 8 terminado

---
+++ tp5ej8.py
@@ -0,0 +1,51 @@
+################
+# Gimenez Fernando - @fergim92
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+from errores import no_numero_int
+
+def cifrado(texto, mover):
+    codificado = []
+    
+    for letra in texto:
+        if letra == " ":
+            codificado.append(" ")
+            continue
+        numero_letra = ord(letra)
+        numero_letra_modi = numero_letra + mover
+        letra_modi = chr(numero_letra_modi)
+        codificado.append(letra_modi)
+        
+    texto = "".join(codificado)  #convertimos la lista en string
+    return texto
+
+def decifrado(texto, mover):
+    codificado = []
+    
+    for letra in texto:
+        if letra == " ":
+            codificado.append(" ")
+            continue
+        numero_letra = ord(letra)
+        numero_letra_modi = numero_letra - mover
+        letra_modi = chr(numero_letra_modi)
+        codificado.append(letra_modi)
+        
+    texto = "".join(codificado)  #convertimos la lista en string
+    return texto
+    
+    
+def prueba():
+    texto = input('Digite un texto para codificarlo: ')
+    mover = input('Cuantas posiciones desea rotar el abecedario para cifrar el mensaje: ')
+    mover = no_numero_int(mover)
+    
+    texto_cifrado = cifrado(texto, mover)
+    texto_decifrado = decifrado(texto_cifrado, mover)
+    
+    print(f'Mensaje cifrado: {texto_cifrado}')
+    print(f'Mensaje decifrado: {texto_decifrado}')
+                            
+    
+if __name__ == "__main__":
+    prueba()
